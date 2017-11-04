56d7727df31d6b62da8c8b3b6607cb176221b202
Ian Henderson <iancurtish@gmail.com>
2017-11-04 16:58:22 -0700

    fixed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ["DEBUG"])
 
-ALLOWED_HOSTS = ["ianhenderson.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["django-portfolio-site.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
