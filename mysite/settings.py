eba79a2bb1ec74637c293457fbf9ab49d721585e
Ian Henderson <iancurtish@gmail.com>
2017-11-07 13:46:55 -0800

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/var/www/example.com/static/'
+STATIC_ROOT = os.path.join(os.getcwd(), "staticfiles")
 
 STATIC_URL = '/static/'
 
